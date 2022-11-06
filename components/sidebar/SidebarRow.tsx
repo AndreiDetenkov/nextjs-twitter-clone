2a272934c10bfc65f246120cd83585b97145e8c7
AndreiDetenkov <a.detenkov@gmail.com>
2022-11-06 23:15:46 +0600

    feat: styled sidebar

--- components/sidebar/SidebarRow.tsx
+++ components/sidebar/SidebarRow.tsx
@@ -1,13 +1,27 @@
-import React, {SVGProps} from "react";
+import React, { SVGProps } from "react";
+import tw from "tailwind-styled-components";
 
 type Props = {
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
   title: string;
 };
 
+const Row = tw.div`
+  flex
+  items-center
+  space-x-2
+  px-4
+  py-3
+  rounded-full
+  hover:bg-gray-100
+  cursor-pointer
+  transition-all
+  duration-200
+`;
+
 const SidebarRow = ({ Icon, title }: Props) => {
   return (
-    <div className="flex items-center space-x-2 px-4 py-3 rounded-full hover:bg-gray-100 cursor-pointer transition-all duration-200">
+    <div>
       <Icon className="w-6 h-6" />
       <p>{title}</p>
     </div>
