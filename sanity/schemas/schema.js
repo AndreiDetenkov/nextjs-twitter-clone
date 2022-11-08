db02379a61f911ead89ef5746a2d80a656e7838d
AndreiDetenkov <a.detenkov@gmail.com>
2022-11-08 16:26:31 +0600

    feat: small updates

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -4,9 +4,7 @@ import schemaTypes from "all:part:@sanity/base/schema-type";
 import tweet from "./tweet";
 import comment from "./comment";
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
   name: "default",
   types: schemaTypes.concat([tweet, comment]),
 });
